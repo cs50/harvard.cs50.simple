0272a52ee28c36264576962b1a38034990e76312
David J. Malan <malan@harvard.edu>
2016-03-10 01:24:18 -0500

    changed double quotes to single quotes

--- c9.ide.cs50.theme/plugin.js
+++ c9.ide.cs50.theme/plugin.js
@@ -2,11 +2,11 @@ define(function(require, exports, module) {
 
     // APIs consumed
     main.consumes = [
-        "commands", "layout", "menus", "Plugin", "settings", "ui"
+        'commands', 'layout', 'menus', 'Plugin', 'settings', 'ui'
     ];
 
     // APIs provided
-    main.provides = ["c9.ide.cs50.theme"];
+    main.provides = ['c9.ide.cs50.theme'];
 
     // plugin
     return main;
@@ -17,66 +17,66 @@ define(function(require, exports, module) {
     function main(options, imports, register) {
 
         // instantiate plugin
-        var plugin = new imports.Plugin("CS50", main.consumes);
+        var plugin = new imports.Plugin('CS50', main.consumes);
 
         // button for menu
         var button = null;
 
         // when plugin is loaded
-        plugin.on("load", function() {
+        plugin.on('load', function() {
 
             // create button
             button = new imports.ui.button({
-                "command": "toggleTheme",
-                "skin": "c9-menu-btn",
-                "tooltip": "Toggle Theme",
-                "visible": true
+                'command': 'toggleTheme',
+                'skin': 'c9-menu-btn',
+                'tooltip': 'Toggle Theme',
+                'visible': true
             });
 
             // register command for button
             imports.commands.addCommand({
                 exec: toggleTheme,
-                group: "CS50",
-                name: "toggleTheme"
+                group: 'CS50',
+                name: 'toggleTheme'
             }, plugin);
 
             // load CSS for button
-            imports.ui.insertCss(require("text!./styles.css"), options.staticPrefix, plugin);
+            imports.ui.insertCss(require('text!./styles.css'), options.staticPrefix, plugin);
 
             // style button
             styleButton();
             
             // re-style button whenever theme changes
-            imports.settings.on("user/general/@skin", function(value) {
+            imports.settings.on('user/general/@skin', function(value) {
                 styleButton();
             }, plugin);
 
             // insert button into menu
             imports.ui.insertByIndex(imports.layout.findParent({
-                name: "preferences"
+                name: 'preferences'
             }), button, 0, plugin);
         });
 
         // when plugin is unloaded
-        plugin.on("unload", function() {
+        plugin.on('unload', function() {
             button = null;
         });
 
         // register plugin
         register(null, {
-            "c9.ide.cs50.theme": plugin
+            'c9.ide.cs50.theme': plugin
         });
 
         /**
          * Styles button based on current theme.
          */
         function styleButton() {
-            var theme = imports.settings.get("user/general/@skin");
-            if (theme === "dark" || theme === "dark-gray" || theme === "flat-dark") {
-                button.setAttribute("class", "cs50-theme-dark");
+            var theme = imports.settings.get('user/general/@skin');
+            if (theme === 'dark' || theme === 'dark-gray' || theme === 'flat-dark') {
+                button.setAttribute('class', 'cs50-theme-dark');
             }
             else {
-                button.setAttribute("class", "cs50-theme-light");
+                button.setAttribute('class', 'cs50-theme-light');
             }
         }
         
@@ -84,13 +84,13 @@ define(function(require, exports, module) {
          * Toggles theme from dark to light or from light to dark.
          */
         function toggleTheme() {
-            if (button.getAttribute("class") === "cs50-theme-dark") {
-                imports.layout.resetTheme("flat-light", "ace");
-                imports.settings.set("user/ace/@theme", "ace/theme/cloud9_day");
+            if (button.getAttribute('class') === 'cs50-theme-dark') {
+                imports.layout.resetTheme('flat-light', 'ace');
+                imports.settings.set('user/ace/@theme', 'ace/theme/cloud9_day');
             }
             else {
-                imports.layout.resetTheme("flat-dark", "ace");
-                imports.settings.set("user/ace/@theme", "ace/theme/cloud9_night");
+                imports.layout.resetTheme('flat-dark', 'ace');
+                imports.settings.set('user/ace/@theme', 'ace/theme/cloud9_night');
             }
         }
     }
